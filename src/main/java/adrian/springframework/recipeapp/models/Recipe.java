7b5d88f3abfd95d8bbba5ecbebca9db7ebdf4189
skillexe10 <skillexe10@o2.pl>
2020-02-23 16:54:58 +0100

    Database initialization Fixed bug

--- src/main/java/adrian/springframework/recipeapp/models/Recipe.java
+++ src/main/java/adrian/springframework/recipeapp/models/Recipe.java
@@ -33,7 +33,7 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
     joinColumns = @JoinColumn(name = "recipe_id"),
     inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> category;
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -131,11 +131,11 @@ public class Recipe {
         this.difficulty = difficulty;
     }
 
-    public Set<Category> getCategory() {
-        return category;
+    public Set<Category> getCategories() {
+        return categories;
     }
 
-    public void setCategory(Set<Category> category) {
-        this.category = category;
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (name) VALUES ('Polish');
+INSERT INTO category (name) VALUES ('Mexican');
+INSERT INTO category (name) VALUES ('Italian');
+INSERT INTO category (name) VALUES ('American');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Spoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
